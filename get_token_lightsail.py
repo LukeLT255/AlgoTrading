48b7e36971a61c56b4eebfe4831759a6088a7d4f
LukeT <lukelt25@gmail.com>
2022-01-07 15:39:56 -0600

    updated path for lightsail chromedriver added

--- get_token_lightsail.py
+++ get_token_lightsail.py
@@ -4,6 +4,6 @@ import config
 
 from selenium import webdriver
 
-with webdriver.Chrome(executable_path='/home/ec2-user/AlgoTrading/chromedriver') as driver:
+with webdriver.Chrome(executable_path='/home/ec2-user/AlgoTrading/linuxChromeDriverFile/chromedriver') as driver:
         c = auth.client_from_login_flow(
             driver, config.api_key, config.redirect_uri, config.token_path)
