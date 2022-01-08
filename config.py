a571b10dcabca86d8f5d25fea5a4ad92db232b06
LukeT <lukelt25@gmail.com>
2022-01-07 23:27:24 -0600

    config updated

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 api_key = 'YQ844XGKN5WIVC04YE47ALML9EKRAAVD@AMER.OAUTHAP'
-token_path = '/Users/luketyson/PycharmProjects/AlgoTrading/token.pickle'
-# token_path = '/home/ec2-user/AlgoTrading/token.pickle'
+# token_path_local = '/Users/luketyson/PycharmProjects/AlgoTrading/token.pickle'
+token_path_lightsail = '/home/ec2-user/AlgoTrading/token.pickle'
 redirect_uri = 'https://localhost'
 account_id = 253530748
