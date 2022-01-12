49d05bbe4fb7ae239a823c172c8042e90d6b6ab2
LukeT <lukelt25@gmail.com>
2022-01-12 10:36:15 -0600

    test

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,13 @@
-# import R2D2
+
 import cronitor
+import R2D2
 import config
 
 cronitor.api_key = config.cron_key
 
 @cronitor.job('trading-bot')
 def runR2D2():
-    print('job ran')
+    R2D2.everyMarketOpen()
 
 if __name__ == '__main__':
     runR2D2()
