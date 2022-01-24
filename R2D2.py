4dafb042d2237edc40d2803cd8e6eead8ee01074
LukeT <lukelt25@gmail.com>
2022-01-24 08:40:06 -0600

    R2D2 updated

--- R2D2.py
+++ R2D2.py
@@ -36,8 +36,8 @@ def make_webdriver():
 client = tda.auth.easy_client(
     config.api_key,
     config.redirect_uri,
-    # config.token_path_lightsail,
-    config.token_path_local,
+    config.token_path_lightsail,
+    # config.token_path_local,
     make_webdriver)
 
 
