006cd854cf62caf1029dc2dca8f4729a991ebe63
LukeT <lukelt25@gmail.com>
2022-03-23 14:07:04 -0400

    Added logging

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ token_path_lightsail = '/home/ec2-user/AlgoTrading/token.pickle'
 redirect_uri = 'https://localhost'
 account_id = 253530748
 cron_key = '6eea94b448e642e6a75ea98daa2395a4'
-max_shares = 5.0
