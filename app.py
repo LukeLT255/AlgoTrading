eb3bf4d51ca5dd5c8bd3c76442ca6082c0a38f02
LukeT <lukelt25@gmail.com>
2022-04-01 12:18:22 -0400

    added postgres

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import gunicorn
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://idfgpxivkpzxxy:73f3fe559854c44dfbd33a1f5e82b5b0a49db5acf65e8889bbff47b48b0e79a3@ec2-54-173-77-184.compute-1.amazonaws.com:5432/dgg38c6arqqie'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://idfgpxivkpzxxy:73f3fe559854c44dfbd33a1f5e82b5b0a49db5acf65e8889bbff47b48b0e79a3@ec2-54-173-77-184.compute-1.amazonaws.com:5432/dgg38c6arqqie'
 db = SQLAlchemy(app)
 
 class Account(db.Model):
