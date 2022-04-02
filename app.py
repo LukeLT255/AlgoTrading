2e0af10de72469209b1623ef2efbecae4a7acd59
LukeT <lukelt25@gmail.com>
2022-04-02 13:17:29 -0400

    reverted to postgres

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import gunicorn
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
 db = SQLAlchemy(app)
 
 class Account(db.Model):
