018e42751d467ef9d51fa99f52099c940e47667b
LukeT <lukelt25@gmail.com>
2022-04-03 15:43:27 -0400

    added script

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 api_key = 'YQ844XGKN5WIVC04YE47ALML9EKRAAVD@AMER.OAUTHAP'
-token_path_local = '/Users/luketyson/PycharmProjects/AlgoTrading/token.pickle'
-token_path_ubuntu = '/home/ubuntu/AlgoTrading/token.pickle'
+# token_path_local = '/Users/luketyson/PycharmProjects/AlgoTrading/token.pickle'
+# token_path_ubuntu = '/home/ubuntu/AlgoTrading/token.pickle'
+token_path = os.path.join(basedir, 'token.pickle')
 redirect_uri = 'https://localhost'
 account_id = 253530748
 cron_key = '6eea94b448e642e6a75ea98daa2395a4'
